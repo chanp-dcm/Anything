aef537a6fcd645586da6a9044cf0fa564f2cfaab
Tomonori Hayashi <hyshtmnr6239dcm@gmail.com>
2018-06-06 18:11:31 +0900

    add sample

--- Python/QiitaAPI/get_ranking_users.py
+++ Python/QiitaAPI/get_ranking_users.py
@@ -1,17 +1,21 @@
 import urllib3
 from bs4 import BeautifulSoup
-import urllib.request
 
 url = 'https://qiita.com/'
 
-response = urllib.request.urlopen(url)
-html = response.read().decode('utf-8')
+import requests
+res = requests.get(url)
+res.raise_for_status()
+html = res.text
+print(html)
 
+# response = request.urlopen(url)
+# html = response.read().decode('utf-8')
+#
 soup = BeautifulSoup(html, "html.parser")
 
 title_tag = soup.find_all('div', attrs={'data-hyperapp-app':'UserRanking'})[0].get('data-hyperapp-props')
 data_list = title_tag.replace('"', '').replace('{', '').split(',')
-print(data_list)
 for index, data in enumerate(data_list):
     if 'urlName' in data:
         print(data.replace('}', '').split(':')[1])
